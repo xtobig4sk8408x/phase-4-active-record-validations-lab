55077ea94ebe2801b48d5885ee81d02d04b92aa1
Theodore John Josef <john.josef@aol.com>
2023-01-31 16:06:21 -0800

    commit tree f1b276f0c97bd839905019b350bd7d0316f8b3a8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, exclusion: ["True Facts"]
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    #validates :title, exclusion: ["True Facts"]
+
 end
